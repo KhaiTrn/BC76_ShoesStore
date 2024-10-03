91bb204e5d7da26ed28826964dd50cbbd67dcc7f
KhaiTrn <khai.tran.srp@gmail.com>
2024-10-03 20:55:39 +0700

    update banner js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// index
 async function getData() {
   let promise = await axios({
     url: "https://shop.cyberlearn.vn/api/Product",
@@ -15,12 +16,13 @@ getData();
 
 function renderData(arr) {
   let content = "";
-  let newArr = arr.slice(0, 6);
-  for (let field of newArr) {
-    const { id, name, image } = field;
-    content += `<div class ="col-4 text-center">
-      <img style="width: 100%;" src="${image}" alt="">
-      <h2>${name}</h2>
+  for (let field of arr) {
+    const { id, name, price, shortDescription, image } = field;
+    content += `<div class ="product_item col-3 text-center">
+      <a href=""><img src="${image}" alt=""></a>
+      <h4><a href="#">${name}</a></h4>
+      <p class= "description">${shortDescription}</p>
+      <p class = "text-danger">$ ${price}</p>
     </div>`;
   }
   document.querySelector(".banner_content").innerHTML = content;
