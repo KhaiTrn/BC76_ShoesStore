6dc1954cb0ecce81722469ed65f63af9338f167b
KhaiTrn <khai.tran.srp@gmail.com>
2024-10-04 23:41:29 +0700

    add more relatedProducts js

--- js/detail.js
+++ js/detail.js
@@ -11,6 +11,9 @@ window.onload = function () {
     .then((res) => {
       console.log(res);
       console.log(res.data.content);
+      console.log(res.data.content.relatedProducts);
+      let arr1 = res.data.content.relatedProducts;
+      render(arr1);
       let arr = [res.data.content];
       renderDataDetail(arr);
     })
@@ -18,19 +21,31 @@ window.onload = function () {
       console.log(err);
     });
 };
-
+function render(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let field of [arr[i]]) {
+      const { image } = field;
+      console.log(image);
+    }
+  }
+}
 function renderDataDetail(arr) {
   let content = "";
   for (let field of arr) {
-    const { id, name, description, price, image } = field;
+    const { id, name, description, price, image, size } = field;
 
-    content += `<div class="detail_img"><img src="${image}" alt="">
+    content += `
+    <div class="detail_img"><img src="${image}" alt="">
           </div>
           <div class="detail_info">
-            <h3>${name}</h3>
-            <p>${description}</p>
-            <p>${price}</p>
-          </div>`;
+            <h3 class="text-black mb-4">${name}</h3>
+            <p class="fs-5">${description}</p>
+            <p class="text-danger fs-4">$ ${price}</p>
+            <p class="text-danger fs-4">Available Size</p>`;
+    for (let i of size) {
+      content += `<button class="btn btn-dark mx-1">${i}</button>`;
+    }
+    content += `</div>`;
   }
   document.getElementById("detail_product").innerHTML = content;
 }
