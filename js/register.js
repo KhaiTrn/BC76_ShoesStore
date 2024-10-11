51ee0a02addf7b593f638a2f5c59b1db385ee770
toandang <toandangphuoc9991@gmail.com>
2024-10-11 16:15:20 +0700

    test merge final

--- js/register.js
+++ js/register.js
@@ -1,29 +1,3 @@
-async function getData() {
-  let promise = await axios({
-    url: "https://shop.cyberlearn.vn/api/Product",
-    method: "GET",
-  })
-    .then((res) => {
-      // console.log(res);
-      renderData(res.data.content);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-getData(); // phần này k liên quan bạn nó của trang index
-
-function renderData(arr) {
-  let content = "";
-  let newArr = arr.slice(0, 6);
-  for (let field of newArr) {
-    const { id, image } = field;
-    content += `<div class="col-6">
-      <img src="${image}" alt="">
-    </div>`;
-  }
-  document.querySelector(".container_a").innerHTML = content;
-}
 let arrFormRegister = {}; // từ khúc này nè okay mình đọc hiểu mà k sao
 document.getElementById("form-register").onsubmit = function (event) {
   console.log("hello");
